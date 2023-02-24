98874563a9319eb0a2463d75c3be943a9172b8a2
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2023-02-24 13:48:36 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
+//Map
 function init() {
     let map = new ymaps.Map('geoMap', {
         center: [55.706802, 37.709290],
         zoom: 10
     })
 
-    let placeMark = new ymaps.Placemark([55.706802, 37.709290],{}, {
-
-    })
+    let placeMark = new ymaps.Placemark([55.706802, 37.709290], {}, {})
 
     map.geoObjects.add(placeMark);
 }
 
 ymaps.ready(init)
+
+//Modal
+const modal = document.getElementById('modal')
+const closeBtnModal = document.querySelectorAll('[data-close]')
+const viewModal = document.getElementById('modalOpen')
+
+closeBtnModal.forEach(item => {
+    item.addEventListener('click', closeModal);
+});
+
+viewModal.addEventListener('click', openModal)
+
+function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+}
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+}
